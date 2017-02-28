3df827dca1b34c9a306d6c6b61e4975d24621d98
Brendan Zabarauskas <brendan@cultureamp.com>
2017-02-28 16:38:51 +1100

    Flesh out the EventStore trait some more

--- chronicle_persistence/src/lib.rs
+++ chronicle_persistence/src/lib.rs
@@ -1,11 +1,44 @@
+extern crate futures;
+
+
+use futures::Stream;
+
+
+/// An event with associated metadata that corresponds to how it was stored in
+/// the event store.
+pub struct PersistedEvent<Offset, Event> {
+    /// The global offset of the event in the event store.
+    pub global_offset: Offset,
+    /// The offset within a single stream.
+    pub stream_offset: Offset,
+    /// The event that was stored by the client of the event store.
+    pub event: Event,
+}
+
+
+/// A repository for storing historical event logs.
 pub trait EventStore {
-    type Id;
-    type Event;
+    /// The unique identifier of a stream in the event store
+    type StreamId;
 
-    fn write_events<Events>(&self, aggregate_id: &Self::Id, events: Events)
-        where Events: IntoIterator<Item = Self::Event>;
+    /// The type of the offests into the event store and its streams
+    type Offset;
 
-    fn read_events(&self, aggregate_id: &Self::Id) -> Vec<Self::Event>;
+    /// The event store's native serialized form
+    type SerializedEvent;
+
+    /// A lazily loaded stream of events in the event store's native form
+    type EventStream: Stream<Item = PersistedEvent<Self::Offset, Self::SerializedEvent>>;
+
+    /// Persist the events to the event store
+    fn persist_events<Events>(&self,
+                              stream_id: &Self::StreamId,
+                              stream_offset: Self::Offset,
+                              events: Events)
+        where Events: IntoIterator<Item = Self::SerializedEvent>;
+
+    /// Read the events back from the event store
+    fn get_events(&self, stream_id: &Self::StreamId) -> Self::EventStream;
 }
 
 pub trait SnapshotStore {}
